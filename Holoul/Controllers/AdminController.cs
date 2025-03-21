96fe229e0010cf17fb551be1474a76dc8bc0fe86
aboodalmsri <1mraboo0de1@gmail.com>
2025-03-21 22:49:28 +0000

    I add models

---
+++ Holoul/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Holoul.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult categories()
+        {
+            return View();
+        }
+        public IActionResult feedback()
+        {
+            return View();
+        }
+        public IActionResult problemsandsolutions()
+        {
+            return View();
+        }
+        public IActionResult profile()
+        {
+            return View();
+        }
+        public IActionResult reports()
+        {
+            return View();
+        }
+    }
+}
